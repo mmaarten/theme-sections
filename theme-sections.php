486dc79a51620f9313f16a056773c27f29a17e50
Maarten Menten <mentenmaarten@gmail.com>
2019-03-22 20:42:08 +0100

    merged files

--- theme-sections.php
+++ theme-sections.php
@@ -15,23 +15,66 @@ Domain Path:  /languages
 namespace theme\sections;
 
 define( 'THEME_SECTIONS_FILE', __FILE__ );
+defined( 'THEME_SECTIONS_POST_TYPE' ) or define( 'THEME_SECTIONS_POST_TYPE', 'section' );
 
-function load()
+function init()
 {
 	$theme = wp_get_theme();
 
-	if ( $theme && $theme->template == 'theme' ) 
-	{
-		require_once plugin_dir_path( THEME_SECTIONS_FILE ) . 'load.php';
-	}
-
-	else
+	if ( ! $theme || $theme->template != 'theme' ) 
 	{
 		add_action( 'admin_notices', 'theme\sections\dependency_notice' );
+
+		return;
 	}
+
+	add_action( 'init'        , 'theme\sections\register_post_type' );
+	add_action( 'widgets_init', 'theme\sections\widgets_init' );
 }
 
-add_action( 'plugins_loaded', 'theme\sections\load' );
+add_action( 'plugins_loaded', 'theme\sections\init' );
+
+function register_post_type()
+{
+	\register_post_type( THEME_SECTIONS_POST_TYPE, array
+	(
+		'labels' => array
+		(
+			'name'               => _x( 'Sections', 'post type general name', 'theme-sections' ),
+			'singular_name'      => _x( 'Section', 'post type singular name', 'theme-sections' ),
+			'menu_name'          => _x( 'Sections', 'admin menu', 'theme-sections' ),
+			'name_admin_bar'     => _x( 'Section', 'add new on admin bar', 'theme-sections' ),
+			'add_new'            => _x( 'Add New', 'section', 'theme-sections' ),
+			'add_new_item'       => __( 'Add New Section', 'theme-sections' ),
+			'new_item'           => __( 'New Section', 'theme-sections' ),
+			'edit_item'          => __( 'Edit Section', 'theme-sections' ),
+			'view_item'          => __( 'View Section', 'theme-sections' ),
+			'all_items'          => __( 'All Sections', 'theme-sections' ),
+			'search_items'       => __( 'Search Sections', 'theme-sections' ),
+			'parent_item_colon'  => __( 'Parent Sections:', 'theme-sections' ),
+			'not_found'          => __( 'No sections found.', 'theme-sections' ),
+			'not_found_in_trash' => __( 'No sections found in Trash.', 'theme-sections' )
+		),
+        'description'        => __( 'Description.', 'theme-sections' ),
+		'public'             => false,
+		'show_ui'            => true,
+		'show_in_menu'       => true,
+		'query_var'          => false,
+		'rewrite'            => false,
+		'capability_type'    => 'post',
+		'has_archive'        => false,
+		'hierarchical'       => false,
+		'menu_position'      => null,
+		'supports'           => array( 'title', 'editor' )
+	));
+}
+
+function widgets_init()
+{
+	require_once plugin_dir_path( THEME_SECTIONS_FILE ) . 'includes/section-widget.php';
+}
+
+add_action( 'widgets_init', 'theme\sections\widgets_init' );
 
 function dependency_notice()
 {
